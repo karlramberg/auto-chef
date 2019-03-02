7d84a524cf00c83861914d1a28cc910113952845
karledramberg <kramberg27@gmail.com>
2019-03-02 09:26:53 -0600

    yeet

--- autochef.py
+++ autochef.py
@@ -1,19 +1,43 @@
 # karl ramberg, paul ramberg, trenton morgan
 # pickhacks 2019 - 3/1/19
-def getFood(path):
+from Food import Food
+
+def getTokens(path):
     return open(path).read().splitlines()
 
-meats = getFood("food/meats.txt")
-carbs = getFood("food/carbs.txt")
-vegetables = getFood("food/vegetables.txt")
-fruits = getFood("food/fruits.txt")
-spices = getFood("food/spices.txt")
+diet = []
 
-print(meats)
-print(carbs)
-print(vegetables)
-print(fruits)
-print(spices)
+for x in range(5):
+    if x == 0:
+        filename = "meats.txt"
+    elif x == 1:
+        filename = "carbs.txt"
+    elif x == 2:
+        filename = "vegetables.txt"
+    elif x == 3:
+        filename = "fruits.txt"
+    else:
+        filename = "spices.txt"
 
+    tokens = getTokens("food/" + filename)
 
+    for token in tokens:
+        if x == 0:
+            weight = 0.6
+            category = "meats"
+        elif x == 1:
+            weight = 0.4
+            category = "carbs"
+        elif x == 2:
+            weight = 0.15
+            category = "vegetables"
+        elif x == 3:
+            weight = 0.15
+            category = "fruits"
+        else:
+            weight = 0.05
+            category = "spices"
 
+        diet.append(Food(token, category, weight))
+
+print(diet)
