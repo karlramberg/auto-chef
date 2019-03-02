7d1393927b68e08ec8e1b1f465b0ca101d9f9af1
karledramberg <kramberg27@gmail.com>
2019-03-02 16:59:30 -0600

    go potatoes

--- server.py
+++ server.py
@@ -1,25 +1,62 @@
 # karl ramberg, paul ramberg, trenton morgan, isaih slater
 # pickhacks 2019 - 3/1/2019
 import generator
+import json
 from sanic import Sanic
 from sanic import response
-from sanic.response import json
 
 
 app = Sanic()
+diet = []
 
 @app.route('/peanutbutter')
 async def test(request):
-    return json({'hello': 'world'})
+    return response.json({'hello': 'world'})
 
 @app.route('/')
 async def test(request):
     return await response.file('./website/index.html')
 
-@app.route("/json")
+@app.get("/jsonget")
 def handle_request(request):
     return response.text(generator.newRecipe())
 
-generator.setDiet()
+@app.post("/jsonyes")
+def post_handler(request):
+    ingredients = json.loads(request.body)
+
+    for ingredient in ingredients:
+        for food in diet:
+            if(ingredient["name"] == food.name):
+                food.probability += 0.1
+                food.probability = chomp(food.probability, 0.0, 1.0)
+
+    return response.json({ "received": True, "message": request.json })
+
+@app.post("/jsonmaybe")
+def post_json(request):
+    return response.json({ "received": True, "message": request.json })
+
+@app.post("/jsonno")
+def post_json(request):
+    ingredients = json.loads(request.body)
+
+    for ingredient in ingredients:
+        for food in diet:
+            if(ingredient["name"] == food.name):
+                food.probability -= 0.1
+                food.probability = chomp(food.probability, 0.0, 1.0)
+
+    return response.json({ "received": True, "message": request.json })
+
+def chomp(num, low, high):
+    if num < low:
+        return low
+    elif num > high:
+        return high
+    else:
+        return num
+
+diet = generator.setDiet()
 app.static("/website", "./website")
 app.run("127.0.0.1", port=80)
