52b47f676031d3d15602aea459e527a8f32c4ed8
Trenton Morgan <notnert99@gmail.com>
2019-03-02 08:41:21 -0600

    Added Food class

---
+++ foodClass.py
@@ -0,0 +1,11 @@
+# karl ramberg, paul ramberg, trenton morgan
+# pickhacks 2019 - 3/1/19
+
+class Food:
+    def __init__(self, name, category, weight):
+        self.name = name
+        self.category = catergory
+        self.weight = weight
+
+    def changeWeight(self, newWeight):
+        self.weight = newWeight
