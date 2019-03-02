253340d32c8a0618c6da6961b5fe701450fbe6d1
karledramberg <kramberg27@gmail.com>
2019-03-02 11:31:42 -0600

    json requests

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+# karl ramberg, paul ramberg, trenton morgan, isaih slater
+# pickhacks 2019 - 3/1/2019
+
 from sanic import Sanic
 from sanic import response
 from sanic.response import json
@@ -11,6 +14,10 @@ async def test(request):
 @app.route('/')
 async def test(request):
     return await response.file('./website/index.html')
+
+@app.route('/json')
+def post_json(request):
+    return json("received": True, "message": request.json)
 
 app.static("/website", "./website")
 app.run("127.0.0.1", port=80)
